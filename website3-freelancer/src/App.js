14e69ad887fb16373d997527af4d9c721b1b5ad2
Ambili Cheran <ambili.cheran@gmail.com>
2024-01-22 11:22:37 +0100

    rename

---
+++ website3-freelancer/src/App.js
@@ -0,0 +1,8 @@
+import "./Components/Navigation/NavigationBar";
+import NavigationBar from "./Components/Navigation/NavigationBar";
+
+const App = () => {
+  return <NavigationBar />;
+};
+
+export default App;
---
+++ website3-freelancer/src/Components/Navigation/NavigationBar.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./NavigationBar.css";
+
+const NavigationBar = () => {
+  return (
+    <nav>
+      <li>START BOOTSTRAP</li>
+      <li>PORTFOLIO</li>
+      <li>ABOUT</li>
+      <li>CONTACT</li>
+    </nav>
+  );
+};
+
+export default NavigationBar;
